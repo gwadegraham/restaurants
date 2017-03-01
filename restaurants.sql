3a65d0699b9a6ff9607e34801054a3031dcc7166
Garrett Graham <garrett@garretts-mbp.attlocal.net>
2017-03-01 00:40:57 -0600

    fixed constraints and added first insertion into Restaurant table

--- restaurants.sql
+++ restaurants.sql
@@ -14,7 +14,7 @@ type ENUM('ap', 'en', 'ds'));
 //Creating MenuItem Table
 CREATE TABLE MenuItem(
 itemNo INTEGER PRIMARY KEY,
-restaurantNo INTEGER, restaurantIDd INTEGER,
+restaurantNo INTEGER, dishNo INTEGER,
 FOREIGN KEY (restaurantNo) REFERENCES Restaurant (restaurantID) ON DELETE RESTRICT,
 FOREIGN KEY (dishNo) REFERENCES Dish (dishNo) ON DELETE SET NULL,
 price DECIMAL(4,2) CHECK (price in (price<= 50.00 and price>=5.00)));
@@ -22,8 +22,12 @@ price DECIMAL(4,2) CHECK (price in (price<= 50.00 and price>=5.00)));
 //Creating FoodOrder Table
 CREATE TABLE FoodOrder(
 orderNo INTEGER PRIMARY KEY,
-restaurantNo INTEGER, dishNo INTEGER,
-FOREIGN KEY (restaurantNo) REFERENCES Restaurant (restaurantID) ON DELETE RESTRICT,
-FOREIGN KEY (dishNo) REFERENCES Dish (dishNo) ON DELETE SET NULL,
+itemNo INTEGER,
+FOREIGN KEY (itemNo) REFERENCES MenuItem (itemNo) ON DELETE CASCADE,
 dt DATE CHECK (date BETWEEN 2017-01-01 AND dateTo),
 ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+
+//Restaurant Table Inserts
+    //first insert
+    INSERT INTO Restaurant (restaurantID, restaurantName, type, city)
+    VALUES ('0', 'Tasty Thai', 'Asian', 'Dallas');
